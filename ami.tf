9e0c3413c6a188d1a36c00b185ffbe116f9a3737
Madhu427 <madhusudhanachary.k@gmail.com>
2022-04-30 00:36:40 +0530

    source_instance_id

--- ami.tf
+++ ami.tf
@@ -2,7 +2,7 @@ resource "aws_ami_from_instance" "ami"
 #depends on is used for creation of ami only after installation of instance
   depends_on = [null_resource.app_deploy]
   name               = "${var.COMPONENT}-${var.APP_VERSION}"
-  source_instance_id = "aws_ami_ami.id"
+  source_instance_id = aws_instance.ami.id
   tags = {
     Name = "${var.COMPONENT}-${var.APP_VERSION}"
   }
